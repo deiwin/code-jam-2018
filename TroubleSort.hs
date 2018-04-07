e82d6d571a8d55bfe14586a989e8d4ea9521b06e
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2018-04-07 17:57:54 +0300

    Revert "Prepare TroubleSort for submission"
    This reverts commit 23257c636455a6dcd8126a8bc8f12d6c00ec3175.

--- TroubleSort.hs
+++ TroubleSort.hs
@@ -1,3 +1,8 @@
+module TroubleSort where
+
+import Pipes
+import qualified Pipes.Prelude as Pipes
+
 import Data.List (sort)
 type ErrorIndex = Int
 
@@ -26,17 +31,20 @@ getErrorIndex = getErrorIndex' 0
 solveCase :: Int -> [Int] -> Maybe ErrorIndex
 solveCase _ = getErrorIndex . (mapTuple sort) . unzip . split
 
-solve' :: Int -> Int -> IO ()
+solve' :: Monad m => Int -> Int -> Pipe String String m ()
 solve' 0 nrOfTestCases = return ()
 solve' testCasesLeft nrOfTestCases = do
-    length <- getLine
-    list <- getLine
+    length <- await
+    list <- await
     let solution = maybe "OK" show (solveCase (read length) (map read (words list)))
-     in putStrLn $ "Case #" ++ (show $ nrOfTestCases - testCasesLeft + 1) ++ ": " ++ solution
+     in yield $ "Case #" ++ (show $ nrOfTestCases - testCasesLeft + 1) ++ ": " ++ solution
     solve' (testCasesLeft - 1)  nrOfTestCases
 
-main :: IO ()
-main = do
-    nrOfTestCasesStr <- getLine
+solve :: Monad m => Pipe String String m ()
+solve = do
+    nrOfTestCasesStr <- await
     let nrOfTestCases = read nrOfTestCasesStr
      in solve' nrOfTestCases nrOfTestCases
+
+main :: IO ()
+main = runEffect $ Pipes.stdinLn >-> solve >-> Pipes.stdoutLn
