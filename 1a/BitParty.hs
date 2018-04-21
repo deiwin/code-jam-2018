31fe7ef624bc9f2c972933b6e79b9e2122e26869
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2018-04-21 21:43:36 +0300

    Revert "Prepare Bit Party for re-submission"
    This reverts commit b9109a5f3e1791d30cdb586284af78a536c6396a.

--- 1a/BitParty.hs
+++ 1a/BitParty.hs
@@ -1,3 +1,7 @@
+module BitParty where
+
+import Pipes
+import qualified Pipes.Prelude as Pipes
 import Control.Monad
 import Data.List
 import Data.Ord
@@ -7,10 +11,10 @@ data Cashier = Cashier { maxBits :: Integer
                        , packingTime :: Integer
                        } deriving (Show, Eq)
 
-readCashiers :: Int -> IO [Cashier]
+readCashiers :: Monad m => Int -> Pipe String String m [Cashier]
 readCashiers rowsToRead =
     foldM fn [] [1..rowsToRead]
-        where fn acc id = do line <- getLine
+        where fn acc id = do line <- await
                              let (maxBits:scanTime:packingTime:_) = map read $ words line
                                  timeWithNextBit = fromIntegral packingTime + scanTime
                               in return $ Cashier { maxBits=maxBits
@@ -39,18 +43,21 @@ binarySearch pred start end
              then binarySearch pred start midway
              else binarySearch pred (midway + 1) end
 
-solve' :: Int -> Int -> IO ()
+solve' :: Monad m => Int -> Int -> Pipe String String m ()
 solve' 0 nrOfTestCases = return ()
 solve' testCasesLeft nrOfTestCases = do
-    line <- getLine
+    line <- await
     let (robotCount:bitCount:cashierCount:_) = map read $ words line
     cashiers <- readCashiers cashierCount
     let solution = show $ binarySearch (\x -> canSolve x robotCount (fromIntegral bitCount) cashiers) 0 (10^9)
-    putStrLn $ "Case #" ++ show (nrOfTestCases - testCasesLeft + 1) ++ ": " ++ solution
+    yield $ "Case #" ++ show (nrOfTestCases - testCasesLeft + 1) ++ ": " ++ solution
     solve' (testCasesLeft - 1)  nrOfTestCases
 
-main :: IO ()
-main = do
-    nrOfTestCasesStr <- getLine
+solve :: Monad m => Pipe String String m ()
+solve = do
+    nrOfTestCasesStr <- await
     let nrOfTestCases = read nrOfTestCasesStr
      in solve' nrOfTestCases nrOfTestCases
+
+main :: IO ()
+main = runEffect $ Pipes.stdinLn >-> solve >-> Pipes.stdoutLn
