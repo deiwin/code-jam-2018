8bf4ce674b9ca3416a44f26f8dab83cb908d8d92
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2018-05-27 13:35:25 +0300

    Test expectedColor

--- 2/GridceptionTest.hs
+++ 2/GridceptionTest.hs
@@ -43,4 +43,27 @@ unitTests = testGroup "Unit tests"
           "Case #4: 4",
           "Case #5: 8"
       ]
+    , testCase "expectedColor" $ do
+        expectedColor "BBBB" (1, 1) (1, 1) @?= 'B'
+        let b = ((1, 1), (2, 4))
+        array b (zip (range b) $ map (expectedColor "BWBW" (1, 2)) $ range b) @?=
+            array b [ ((1, 1), 'B')
+                    , ((1, 2), 'B')
+                    , ((1, 3), 'W')
+                    , ((1, 4), 'W')
+                    , ((2, 1), 'B')
+                    , ((2, 2), 'B')
+                    , ((2, 3), 'W')
+                    , ((2, 4), 'W')
+                    ]
+        array b (zip (range b) $ map (expectedColor "BBBB" (1, 2)) $ range b) @?=
+            array b [ ((1, 1), 'B')
+                    , ((1, 2), 'B')
+                    , ((1, 3), 'B')
+                    , ((1, 4), 'B')
+                    , ((2, 1), 'B')
+                    , ((2, 2), 'B')
+                    , ((2, 3), 'B')
+                    , ((2, 4), 'B')
+                    ]
   ]
